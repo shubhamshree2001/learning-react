9bd6444037a64db267ade7e8d92aee8e23fee8d5
shubhamshree2001 <shubhamshree5@gmail.com>
2026-05-01 01:40:29 +0530

    creating react project through vite

---
+++ 01vitereact/src/App.jsx
@@ -0,0 +1,11 @@
+function App() {
+
+  return (
+   <h1> react with vite</h1>
+  )
+}
+
+export default App
+
+
+// we need to bring javascript file/code inside the script inside the html then only it will manipulate
